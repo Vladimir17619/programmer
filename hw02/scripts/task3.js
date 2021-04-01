8769d74ab028ab00df9b1fa5e29f849604de6417
Vladimir <elensn85@gmail.com>
2021-04-01 15:48:18 +0300

    home work 2

---
+++ hw02/scripts/task3.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = -5;
+prompt(a);
+prompt(b);
+if (a > 0 && b > 0) {
+    alert(a - b);
+}
+else (a < 0 && b < 0){
+    alert(a * b);
+}
+
+
---
+++ hw02/task3.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 5;
+if (a > 0, b > 0)
+    alert(a - b);
+else (a < 0, b < 0)
+alert(a * b);
+

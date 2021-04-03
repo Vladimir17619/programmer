d842232e93a190a7abcca2346406e271f5e240f3
Vladimir <elensn85@gmail.com>
2021-04-03 17:44:52 +0300

    home work finished

---
+++ hw02/task5.js
@@ -0,0 +1,22 @@
+x = prompt();
+y = prompt();
+function sum(x, y) {
+    return (x + y)
+}
+z = sum(x, y);
+alert(z);
+function dif(x, y) {
+    return (x - y)
+}
+z = dif(x, y);
+alert(z);
+function mul(x, y) {
+    return (x * y)
+}
+z = mul(x, y);
+alert(z);
+function div(x, y) {
+    return (x / y)
+}
+z = div(x, y);
+alert(z);
